ad2e7451ee03a5320905e3bf3e069b67c40aa274
unknown <mariam.i.awad@gmail.com>
2019-07-12 18:39:24 +0300

    gitignore is now working

--- Exercises/10.1/TestTime.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class TestTime{
-    public static void main (String [] args) {
-        Time currentTime = new Time();
-        System.out.println(currentTime.hours + ":" + currentTime.minutes + ":" + currentTime.seconds);
-        Time time = new Time(555550000);
-        System.out.println(time.hours + ":" + time.minutes + ":" + time.seconds);       
-    }
-}
--- Exercises/JavaExercises/Chapter1/1.12/AverageSpeedKilometer.java
+++
@@ -1,9 +0,0 @@
-public class AverageSpeedKilometer {
-public static void main (String[] args){
-	
-	
-	System.out.printf("%f\n",(24*1.6)/(1+(40/60.0)+(35/(60*60.0))));
-	
-		
-}
-}
