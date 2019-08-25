58910de42da1b2a46feb7abcfa2880f2a28d0270
mariamiawad <mariam.i.awad@gmail.com>
2019-08-25 19:53:34 +0200

    Book Exercises

--- CodingExercises/src/Test.java
+++ CodingExercises/src/Test.java
@@ -1,39 +1,20 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Test
-{
-    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
-    public static int generalizedGCD(int num, int[] arr)
-    {
-      int max = 0;
-      for (int i = 0, j = 2;i< arr.length ;i++, j++){
-          
-          max = getGCD(arr[i], arr[i-1]);
-      } 
-      return max;
-    }
-
-    private static int getGCD(int a, int b) {
-        if(a == 0){
-            return b;
-        }
-       
-        	return getGCD(a, b%a);
-        
-    }
-public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	int n = scanner.nextInt();
-	int [] arr = new int [n];
-	for (int i = 0; i < arr.length; i++) {
-		arr[i] = scanner.nextInt();
-	}
-	int x = generalizedGCD(n, arr);
-	
-}
-	
-        
-    
-    // METHOD SIGNATURE ENDS
+public class Test {
+ public static void main(String[] args) {
+ 
+  double pi = 0;
+ 
+  for (int i = 1; i <= 100000; i++) {
+   pi += Math.pow(-1, i + 1) / (2 * i - 1);
+   if (i == 10000) {
+    System.out.println("Pi at i = 10000 is " + pi*4);
+   } else if (i == 20000) {
+    System.out.println("Pi at i = 20000 is " + pi*4);
+   } else if (i == 100000) {
+    System.out.println("Pi at i = 100000 is " + pi*4);
+   }
+ 
+  }
+   System.out.println("Java's pi is " + Math.PI);
+ 
+ }
 }
