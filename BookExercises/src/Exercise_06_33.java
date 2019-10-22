da5afdf17f5ede085265e5dd16ba484da7998d35
mariamiawad <mariam.i.awad@gmail.com>
2019-10-22 15:19:14 +0200

    Exercise 6.33

---
+++ BookExercises/src/Exercise_06_33.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Exercise_06_33 {
+
+	public Exercise_06_33() {
+	}
+	public static void main(String[] args) {
+		long milliSeconds = System.currentTimeMillis();
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm:ss");    
+		Date resultdate = new Date(milliSeconds);
+		System.out.println("Current date and time is " +sdf.format(resultdate));
+	}
+
+}
