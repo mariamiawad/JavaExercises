e265ac45f314e5987d13a3f7f8602a88a0b39b69
mariamiawad <mariam.i.awad@gmail.com>
2019-10-27 13:22:19 +0200

    Exercise 6.35

---
+++ BookExercises/src/Exercise_06_35.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise_06_35 {
+
+	public Exercise_06_35() {
+	}
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Enter the side: ");
+		System.out.println("The area of the pentagon is " + area(inputScanner.nextDouble()));
+	}
+	public static double area(double side) {
+		return (5 * Math.pow(side, 2))/(4*(Math.tan(Math.PI/5)));
+		
+	}
+
+}
