9b3ad9f918750467d9f02721d45aadb866b1a76a
mariamiawad <mariam.i.awad@gmail.com>
2019-10-27 19:53:49 +0200

    Exercise 15.8-2

--- MyJavaFX/src/Exercise_15_08_02.java
+++ MyJavaFX/src/Exercise_15_08_02.java
@@ -15,10 +15,11 @@ public static void main(String[] args) {
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane pane = new BorderPane();
 		Text text = new Text(50, 50," ");
-		pane.setOnMouseReleased(e->text.setText("("+e.getX() +","+ e.getY()+")"));
+		pane.setOnMousePressed(e->text.setText("("+e.getX() +","+ e.getY()+")"));
+		pane.setOnMouseReleased(e->text.setText(" "));
 		pane.getChildren().add(text);
 		Scene scene = new Scene(pane, 300, 250);
-		primaryStage.setTitle("Exercise_15_08");
+		primaryStage.setTitle("Exercise_15_08_02");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
