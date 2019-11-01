e9d9cfb4cf1bc3a42bab53c8d3f6274698759f1b
mariamiawad <mariam.i.awad@gmail.com>
2019-11-01 13:18:13 +0200

    Exercise 10.17

---
+++ Book1/src/Exercise_10_17.java
@@ -0,0 +1,28 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class Exercise_10_17 {
+
+	public Exercise_10_17() {
+	}
+
+	public static void main(String[] args) {
+		BigDecimal longMaxValue = new BigDecimal(Long.MAX_VALUE);
+		MathContext mContext = new MathContext(10);
+		BigDecimal n = longMaxValue.sqrt(mContext);
+		int count = 10;
+		while (count > 0) {
+			
+			
+			BigDecimal squared = n.multiply(n);
+			if (squared.compareTo(longMaxValue) > 0) {
+				count--;
+				System.out.println(count + ": " + n + " squared = " + squared);
+			}
+			n = n.add(BigDecimal.ONE);
+		}
+	}
+
+}
