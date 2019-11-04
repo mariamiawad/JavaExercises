dd75795f7cb178697eab4bd544bf7ca4d2c30c63
mariamiawad <mariam.i.awad@gmail.com>
2019-11-04 08:11:18 +0200

    Exercise 6.39

--- Book1/src/Exercise_06_39.java
+++ Book1/src/Exercise_06_39.java
@@ -24,10 +24,9 @@ public class Exercise_06_39 {
 		} else if (onTheSameLine(x0, y0, x1, y1, x2, y2)) {
 			System.out.println("(" + x2 + "," + y2 + ") is one the same line from (" + x0 + "," + y0 + ") to (" + x1
 					+ "," + y1 + ")");
-		}
-		else {
-			System.out.println("(" + x2 + "," + y2 + ") is one the right side of the line from (" + x0 + "," + y0 + ") to (" + x1
-					+ "," + y1 + ")");
+		} else {
+			System.out.println("(" + x2 + "," + y2 + ") is one the right side of the line from (" + x0 + "," + y0
+					+ ") to (" + x1 + "," + y1 + ")");
 		}
 	}
 
@@ -40,16 +39,9 @@ public class Exercise_06_39 {
 	}
 
 	public static boolean onTheLineSegment(double x0, double y0, double x1, double y1, double x2, double y2) {
-		if (onTheSameLine(x0, y0, x1, y1, x2, y2)) {
-			 
-			   if (((x2 < x1 && x2 > x0) || (x2 < x0 && x2 > x1))
-			     && ((y2 < y1 && y2 > y0) || (y2 < y0 && y2 > y1))) {
-			    return true;
-			   }
-			 
-			  }
-			 
-			  return false;
+		return (onTheSameLine(x0, y0, x1, y1, x2, y2)
+				&& (((x2 < x1 && x2 > x0) || (x2 < x0 && x2 > x1)) && ((y2 < y1 && y2 > y0) || (y2 < y0 && y2 > y1))));
 
 	}
+
 }
