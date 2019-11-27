0d96b7b9ef30c837b8ea051966b09a8657fb589c
mariamiawad <mariam.i.awad@gmail.com>
2019-11-27 12:43:18 +0200

    newdatastructure

---
+++ newdatastructure/src/newdatastructure/MyArrayList.java
@@ -0,0 +1,180 @@
+package newdatastructure;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyArrayList<E> implements List<E> {
+
+	static int capacity = 10;
+	E[] arrayList;
+	int size = 0;;
+
+	public MyArrayList() {
+		this.arrayList = (E[]) new Object[capacity];
+	}
+
+	@Override
+	public void add(E e) {
+		if (size >= capacity) {
+			resize();
+		}
+		arrayList[size] = e;
+		size++;
+
+	}
+
+	@Override
+	public void add(int index, E element) {
+
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (size >= capacity) {
+			resize();
+		}
+		arrayList[index] = element;
+		size++;
+
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		for (int i = 0; i < arrayList.length; i++) {
+			if (arrayList[i] == o) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public E get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return arrayList[index];
+
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		for (int i = 0; i < arrayList.length; i++) {
+			if (arrayList[i] == o) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+
+		return size == 0;
+	}
+
+	@Override
+	public E remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		int ind = -1;
+		E returnE = null;
+		for (int i = index; i < arrayList.length; i++) {
+			if (i == index) {
+				ind = i;
+				returnE = arrayList[i];
+				size--;
+				break;
+				
+			}
+
+		}
+		
+		if (ind>-1) {
+			for (int i = ind; i < arrayList.length-1; i++) {
+				arrayList[i]= arrayList[i+1];
+			}	
+		}
+		
+		if (size <= 0.25 * capacity) {
+			resize();
+		}
+		return returnE;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		for (int i = 0; i < arrayList.length; i++) {
+			if (arrayList[i].equals(o)) {
+				remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+
+		}
+		E value = arrayList[index];
+		arrayList[index] = element;
+		return value;
+	}
+
+	@Override
+	public int size() {
+		return size;
+
+	}
+
+	public void resize() {
+		E[] newArray = null;
+
+		if (size <= 0.25 * capacity) {
+			newArray = (E[]) new Object[size];
+			for (int i = 0; i < newArray.length; i++) {
+				newArray[i] = arrayList[i];
+			}
+		} else if (size >= capacity) {
+			newArray = (E[]) new Object[size * 2];
+			for (int i = 0; i < arrayList.length; i++) {
+				newArray[i] = arrayList[i];
+			}
+		}
+		if (size >= 0.25 * capacity || size >= capacity) {
+			this.arrayList = newArray;
+			this.capacity = arrayList.length;
+		}
+
+	}
+
+	public MyIterator<E> iterator() {
+		return new MyIterator();
+	}
+
+	public class MyIterator<E> implements Iterator<E> {
+		int indexIterator = 0;
+
+		public MyIterator() {
+		}
+
+		@Override
+		public boolean hasNext() {
+			return indexIterator < size;
+		}
+
+		@Override
+		public E next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+
+			return (E) arrayList[indexIterator++];
+
+		}
+	}
+
+}
---
+++ newdatastructure/src/test/TestMyArrayList.java
@@ -0,0 +1,230 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+import newdatastructure.MyArrayList;
+import newdatastructure.MyArrayList.MyIterator;
+
+public class TestMyArrayList {
+
+	@Test
+	public void size_AddingTwoInteger_ExpectedTwo() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.size(), 2);
+	}
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void addAtIndex_IndexBigerThanSize_OutOfBoundsExeption() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3,4);
+	}
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void addAtIndex_IndexBigerThanSmallerThanZero_OutOfBoundsExeption() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3,-1);
+		
+	}
+	@Test
+	public void Contains_ContainsAnElement_True() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.contains(2), true);
+	}
+
+	@Test
+	public void Contains_ContainsAnElement_False() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.contains(4), false);
+	}
+	@Test
+	public void Get_GetAnElement_Two() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.get(1), Integer.valueOf(2));
+	}
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void Get_GetIndexSmallerThanZero_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.get(-1);
+	}
+
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void Get_GetIndexSmallerThanSize_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.get(2);
+	}
+
+	@Test
+		public void IndexOf_IndexOfTwo_Two() {
+			MyArrayList<Integer> arrayList = new MyArrayList<>();
+			arrayList.add(1);
+			arrayList.add(2);
+			assertEquals(arrayList.indexOf(2), 1);
+		}
+
+	@Test
+	public void IndexOf_IndexOfObjectNotFound_MinusOne() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.indexOf(3), -1);
+	}
+
+	@Test 
+	public void IndexOf_IndexOfElementIsntFound_MinusOne() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.indexOf(3), -1); 
+	}
+
+	@Test
+	public void IndexOf_IndexOfElementEound_One() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.indexOf(2),1); 
+	}
+
+	@Test
+	public void IsEmpty_ArrayIsEmpty_True() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		assertEquals(arrayList.isEmpty(),true); 
+	}
+	@Test
+	public void IsEmpty_ArrayIsEmpty_False() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.isEmpty(),false); 
+	}
+	@Test
+	public void Remove_ElementFoundNotFound_false() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.remove(Integer.valueOf(0)),false); 
+	}
+	@Test
+	public void Remove_ElementFound_True() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.remove(Integer.valueOf(2)),true); 
+	}
+	
+	@Test
+	public void Remove_ElementFound_Two() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.remove(1),Integer.valueOf(2)); 
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void Remove_IndexBiggerThanSize_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.remove(2);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void Remove_IndexSmallerThanZero_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.remove(-1);
+	}
+	@Test
+	public void Set_SetElementatSpecificLocation_Two() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		assertEquals(arrayList.set(1, Integer.valueOf(0)),Integer.valueOf(2)); 
+	}
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void Set_SetElementatSpecificLocationBiggerThanSize_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.set(3, Integer.valueOf(0));
+	}
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void Set_SetElementatSpecificLocationSmallerThanZero_IndexOutOfBoundException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.set(-1, Integer.valueOf(0));
+	}
+	
+	@Test
+	public void hasNext_ArrayHasNext_True() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		MyArrayList<Integer>.MyIterator<Integer> iterator = arrayList.iterator();
+		assertEquals(iterator.hasNext(), true);
+	}
+	@Test
+	public void hasNext_ArrayHasNext_False() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		
+		MyArrayList<Integer>.MyIterator<Integer>  i = arrayList.iterator();
+		assertEquals(i.hasNext(),false); 
+	}
+	@Test (expected = NoSuchElementException.class)
+	public void hasNext_ArrayNext_NoSuchElementException() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		
+		MyArrayList<Integer>.MyIterator<Integer>  i = arrayList.iterator();
+		i.next();
+	}
+	
+	
+	@Test 
+	public void hasNext_ArrayNext_Two() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		MyArrayList<Integer>.MyIterator<Integer>  i = arrayList.iterator();
+		assertEquals(i.next(), Integer.valueOf(1));
+		assertEquals(i.next(), Integer.valueOf(2));
+	}
+	@Test 
+	public void hasNext_ArrayNext_One() {
+		MyArrayList<Integer> arrayList = new MyArrayList<>();
+		arrayList.add(1);
+		MyArrayList<Integer>.MyIterator<Integer>  i = arrayList.iterator();
+		assertEquals(i.next(), Integer.valueOf(1));
+	}
+	@Test
+	public void Resize_FullArray_ResizeBiggerThanOriginalArrayByTwo() {
+		MyArrayList<Integer> arrayList = new MyArrayList<Integer>();
+		for (int i = 0; i <11; i++) {
+			arrayList.add(i);
+		}
+		for (int i = 0; i < 6; i++) {
+			arrayList.remove(i);
+		}
+		assertEquals(arrayList.size(),5);
+	}
+
+}
