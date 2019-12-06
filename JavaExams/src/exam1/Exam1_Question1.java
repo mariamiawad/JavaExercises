8ce96a40f421a6c6cc6c34bc71ed03644a6258ef
mariamiawad <mariam.i.awad@gmail.com>
2019-12-06 19:18:09 +0200

    Exam Question 1

--- JavaExams/src/exam1/Exam1_Question1.java
+++ JavaExams/src/exam1/Exam1_Question1.java
@@ -5,8 +5,8 @@ public class Exam1_Question1 {
 	public static void main(String[] args) {
 		int n = Math.abs(Integer.parseInt(args[0]));
 		for (int i = 0; i <=n ; i++) {
-			if(Math.pow(i, 2)<=n)
-			System.out.println("Power of " + i + "= " + Math.pow(i, 2));
+			if(Math.pow(2, i)<=n)
+			System.out.println(" 2 Power of " + i + " = " + Math.pow(2, i));
 		}
 	}
 }
