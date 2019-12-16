45838f2f15087cb17f5f358dbde67d06eb925ac0
Khaled Elbanna <engineer.khaled@gmail.com>
2019-12-16 00:23:46 +0000

    Week 15 December Exercises

---
+++ DataStructure/src/sorting/QuickSort.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class QuickSort {
+	public <T extends Comparable<T>> void sort(T[] arr) {
+		// TODO - Implement this method
+	}
+	
+	public <T extends Comparable<T>> void sortWithShuffle(T[] arr) {
+		// TODO - Implement this method
+	}
+
+	public <T extends Comparable<T>> void sortMedianOfThree(T[] arr) {
+		// TODO - Implement this method
+	}
+}
