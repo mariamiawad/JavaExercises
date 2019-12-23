2edf6d0f55b36b620d3aa16985d2f54b6290efe8
mariamiawad <mariam.i.awad@gmail.com>
2019-12-23 21:31:05 +0200

    Quick Sort

---
+++ DataStructure/src/sorting/ContextQuickSort.java
@@ -0,0 +1,84 @@
+package sorting;
+
+import java.util.Random;
+
+
+public class ContextQuickSort {
+	private StrategySorting strategy;
+	public ContextQuickSort(StrategySorting strategy) {
+		this.strategy =strategy;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySort() {
+		Integer[] list = new Integer[100000];
+		generateArray(list);
+		strategy.sort(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortWithSuffle() {
+		Integer[] list = new Integer[100000];
+		generateArray(list);
+		strategy.sortWithShuffle(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortMedianOfThree() {
+		Integer[] list = new Integer[100000];
+		generateArray(list);
+		strategy.sortMedianOfThree(list);
+		return list;
+	}
+	
+	
+	public<T extends Comparable<T>>  Integer[] executeStrategySortSortedArray() {
+		Integer[] list = new Integer[100000];
+		generateArray(list, 0 , 0);
+		strategy.sort(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortWithSuffleSortedArray() {
+		Integer[] list = new Integer[100000];
+		generateArray(list, 0,0);
+		strategy.sortWithShuffle(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortMedianOfThreeSortedArray() {
+		Integer[] list = new Integer[100000];
+		generateArray(list, 0, 0);
+		strategy.sortMedianOfThree(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortEmpty() {
+		Integer[] list = new Integer[100000];
+		strategy.sort(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortWithSuffleEmpty() {
+		Integer[] list = new Integer[100000];
+		strategy.sortWithShuffle(list);
+		return list;
+	}
+	public<T extends Comparable<T>>  Integer[] executeStrategySortMedianOfThreeEmpty() {
+		Integer[] list = new Integer[100000];
+		strategy.sortMedianOfThree(list);
+		return list;
+	}
+	private Integer[] generateArray(Integer [] list, int currentValue, int lastValue) {
+		Random random = new Random();
+		for (int i = 0; i < list.length; i++) {
+			currentValue = random.nextInt(200);
+			lastValue = lastValue + currentValue;
+			list[i] = lastValue;
+		}
+		return list;
+		
+	
+	}
+	private Integer[] generateArray(Integer [] list) {
+		Random random = new Random();
+		for (int i = 0; i < list.length; i++) {
+			list[i] = random.nextInt(1000);
+
+		}
+		return list;
+	}
+}
+
--- DataStructure/src/sorting/QuickSort.java
+++ DataStructure/src/sorting/QuickSort.java
@@ -1,15 +1,79 @@
 package sorting;
 
-public class QuickSort {
+public class QuickSort implements StrategySorting {
 	public <T extends Comparable<T>> void sort(T[] arr) {
-		// TODO - Implement this method
+
+		sort(arr, 0, arr.length - 1);
+
 	}
-	
+
 	public <T extends Comparable<T>> void sortWithShuffle(T[] arr) {
-		// TODO - Implement this method
+		Shuffle shuffle = new Shuffle();
+		shuffle.shuffle(arr);
+		sort(arr);
+	}
+
+	private static <T extends Comparable<T>> int partition(T[] array, int start, int end, T pivot) {
+		int i = start - 1;
+		for (int j = start; j < end; j++) {
+			if (array[j].compareTo(pivot) < 0) {
+				i++;
+				swap(array, i, j);
+			}
+
+		}
+		swap(array, i + 1, end);
+		return i + 1;
+
+	}
+
+	private <T extends Comparable<T>> void sort(T[] array, int start, int end) {
+		if (end <= start) {
+			return;
+
+		}
+		int p = partition(array, start, end, array[end]);
+		sort(array, start, p - 1);
+		sort(array, p + 1, end);
 	}
 
 	public <T extends Comparable<T>> void sortMedianOfThree(T[] arr) {
-		// TODO - Implement this method
+		int start = 0;
+		int end = arr.length-1;
+		sortMedianOfThree(arr, start, end);
 	}
+	
+
+
+	private static <T extends Comparable<T>> void swap(T[] array, int left, int right) {
+		T temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+	}
+	private <T extends Comparable<T>> void sortMedianOfThree(T[] array, int start, int end) {
+		 if(start >= end)
+	            return;
+	         
+	        T pivot = getMedian(array,start, end);
+	        int partition = partition(array,start, end, pivot);
+	         
+	        sortMedianOfThree(array,start, partition-1);
+	        sortMedianOfThree(array,partition+1, end);
+		
+	}
+	 private static  <T extends Comparable<T>> T getMedian(T[] array, int left,int right){
+	        int center = (left+right)/2;
+	         
+	        if(array[left].compareTo(array[center]) > 0 )
+	            swap( array, left,center);
+	         
+	        if(array[left].compareTo(array[right]) > 0 )
+	            swap(array,left, right);
+	         
+	        if(array[center].compareTo(array[right]) > 0)
+	            swap(array,center, right);
+	         
+	        swap(array,center, right);
+	        return array[right];
+	    }
 }
