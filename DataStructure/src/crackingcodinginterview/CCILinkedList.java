fd295fa7b33fd9106bbd463f692493f9a1aa1294
mariamiawad <mariam.i.awad@gmail.com>
2019-12-23 21:37:11 +0200

    Remove Duplicates

--- DataStructure/src/crackingcodinginterview/CCILinkedList.java
+++ DataStructure/src/crackingcodinginterview/CCILinkedList.java
@@ -1,11 +1,29 @@
 package crackingcodinginterview;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import arraylistlinkedlist.List;
+
+import java.util.Set;
 
 public class CCILinkedList {
 	
 	public <E> void removeDuplicates(LinkedList<E> list) {
-		// TODO - Implement this method
+		LinkedList<E> newList = new LinkedList<>();
+		newList.add(list.get(0));
+		for (int i = 1; i < list.size(); i++) {
+			if (!newList.contains(list.get(i))) {
+				newList.add(list.get(i));
+			}
+		}
+		list.clear();
+		for (int i = 0; i < newList.size(); i++) {
+			list.add(newList.get(i));
+		}
 	}
 
 	public static void main(String[] args) {
