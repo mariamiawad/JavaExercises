1553f665bb40347012d432dea276259d0247a9b2
mariamiawad <mariam.i.awad@gmail.com>
2019-12-31 15:35:52 +0200

    BST

--- DataStructure/src/tree/BST.java
+++ DataStructure/src/tree/BST.java
@@ -1,44 +1,140 @@
 package tree;
 
-public class BST<Key extends Comparable<Key>> {
-	
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BST<Key extends Comparable<Key>> implements Iterable<Key> {
+	private Node root = null;
+	ArrayList<Key> list;
 	class Node {
 		Key key;
 		Node left, right;
-		
 		Node(Key key) {
 			this.key = key;
 		}
 	}
-	
+
 	public Node search(Key key) {
-		// TODO - Implement this method
-		return null;
+		Node node = search(root, key);
+		return node;
 	}
-	
+
+	private Node search(Node node, Key key) {
+		Node current = root;
+		if (root.key == null || root.key.compareTo(key) == 0) {
+			return root;
+		} else if (current.key.compareTo(key) > 0) {
+			return search(node.left, key);
+		}
+
+		return search(node.right, key);
+
+	}
+
 	public void insert(Key key) {
-		// TODO - Implement this method
-		
+		insert(root, key);
 	}
-	
+
+	private Node insert(Node root, Key key) {
+		if (root == null) {
+			root = new Node(key);
+		}
+		if (key.compareTo(root.key) < 0) {
+			root.left = insert(root.left, key);
+		} else if (key.compareTo(root.key) > 0) {
+			root.right = insert(root.right, key);
+		}
+		return root;
+
+	}
+
 	public void delete(Key key) {
-		// TODO - Implement this method
-		
+		delete(root, key);
 	}
-	
+
+	private Node delete(Node root,Key key) {
+		if (root == null) {
+			return root;
+		}
+		else if (key.compareTo(root.key)< 0) {
+			root.left = delete(root.left, key);
+		}
+		else if (key.compareTo(root.key)>0) {
+			root.right = delete(root.right, key);
+		}
+		else {
+			if (root.left == null) {
+				return root.right;
+			}
+			else if (root.right == null) {
+				return root.left;
+			}
+			root = minValue(root.right);
+			root.right = delete(root, key);
+		}
+		return root;
+	}
+
 	public Iterable<Key> inorder() {
-		// TODO - Implement this method
-		return null;
+		list = inorder(root);
+		return list;
 	}
+
+	private ArrayList<Key> inorder(Node root) {
+		list = new ArrayList<>();
+		inorder(root.left);
+		list.add(root.key);
+		inorder(root.right);
+		return list;
+
+	}
+
 	
+
 	public Iterable<Key> preorder() {
-		// TODO - Implement this method
-		return null;
+		list = preorder(root);
+		return list;
 	}
-	
+	private ArrayList<Key> preorder(Node root) {
+		list = new ArrayList<>();
+		if (root == null) {
+			return list;
+		}
+		list.add(root.key);
+		preorder(root.left);
+		preorder(root.right);
+		return list;
+
+	}
+
 	public Iterable<Key> postorder() {
-		// TODO - Implement this method
-		return null;
-	}	
+		list = postorder(root);
+		return list;
+	}
+	private ArrayList<Key> postorder(Node root) {
+		list = new ArrayList<>();
+		if (root == null) {
+			return list;
+		}
+		postorder(root.left);
+		postorder(root.right);
+		list.add(root.key);
+		return list;
+
+	}
+
+	private Node minValue(Node root) {
+		
+		if (root.left != null) {
+			return minValue(root.left);
+		}
+
+		return root;
+	}
+
+	@Override
+	public Iterator<Key> iterator() {
+		return list.iterator();
+	}
 
 }
