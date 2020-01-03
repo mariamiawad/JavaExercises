49c8c02647071c245cca02f30fef7518cc2b5bb9
mariamiawad <mariam.i.awad@gmail.com>
2020-01-03 21:19:55 +0200

    Chapter 18 Exercise 14

---
+++ JavaBookExercises/src/chapter18/Exercise_18_14.java
@@ -0,0 +1,28 @@
+package chapter18;
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class Exercise_18_14 {
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		String string = inputScanner.next();
+		Character[] characters = string.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+		System.out.println(getNumberOfUperCaseLetters(characters, 0));
+	}
+
+	private static int getNumberOfUperCaseLetters(Character[] characters, int i) {
+		int x;
+			if (i>= characters.length) {
+				return 0;
+			}
+			
+			else if (Character.isUpperCase(characters[i].charValue()) ) {
+				return 1 + getNumberOfUperCaseLetters(characters, i+1);
+			}
+			else {
+				return getNumberOfUperCaseLetters(characters, i+1);
+			}
+		
+	}
+}
