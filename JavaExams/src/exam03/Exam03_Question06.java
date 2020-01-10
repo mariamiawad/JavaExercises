d7f8268100e9c8cc679e268a2d4fd8b0e1c119da
mariamiawad <mariam.i.awad@gmail.com>
2020-01-10 18:42:39 +0200

    Exam03_Question06

--- JavaExams/src/exam03/Exam03_Question06.java
+++ JavaExams/src/exam03/Exam03_Question06.java
@@ -1,34 +1,29 @@
 package exam03;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
-import mystak.MyStackArrayList;
+import sorting.QuickSort;
 
 public class Exam03_Question06 {
-	public static void main(String[] args) {
-		MyStackArrayList<Character> stackArrayList = new MyStackArrayList<>();
-		Scanner inputScanner = new Scanner(System.in);
-		String brackets = inputScanner.nextLine();
-		char[] ch = brackets.toCharArray();
-		boolean flag = true;
-		for (int i = 0; i < ch.length; i++) {
-			if (ch[i] == '[' || ch[i] == '(' || ch[i] == '{') {
-				stackArrayList.push(ch[i]);
-			} else if (ch[i] == ']' && stackArrayList.peek() == '[') {
-				stackArrayList.pop();
-				flag = true;
-			} else if (ch[i] == ')' && stackArrayList.peek() == '(') {
-				stackArrayList.pop();
-			} else if (ch[i] == '}' && stackArrayList.peek() == '{') {
-				stackArrayList.pop();
-			} else {
-				flag = false;
-				break;
-			}
-
-		}
-
-		System.out.println(flag);
-
+public static void main(String[] args) {
+	Scanner inputScanner = new Scanner(System.in);
+	int n = inputScanner.nextInt();
+	int m = inputScanner.nextInt();
+	Set<Integer> set = new HashSet<>();
+	for (int i = 0; i < n+m; i++) {
+		set.add(inputScanner.nextInt());
 	}
+	List<Integer> list = new ArrayList<>();
+	list.addAll(set);
+	QuickSort sort = new QuickSort();
+	Integer[]array = list.toArray(new Integer[list.size()]);
+	sort.sortMedianOfThree(array);
+	for (int i = 0; i < array.length; i++) {
+		System.out.print(array[i]+" ");
+	}
+}
 }
