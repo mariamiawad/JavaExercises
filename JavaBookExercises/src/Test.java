58515d875f3e36530c0d660523b2542b9394a02e
mariamiawad <mariam.i.awad@gmail.com>
2020-01-22 13:43:23 +0200

    Exercise_12_28 + Exercise_12_29 Modified

--- JavaBookExercises/src/Test.java
+++ JavaBookExercises/src/Test.java
@@ -1,74 +1,50 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public abstract class Test<E extends Object> {
 	public static void main(String[] args) {
-		int[] list = new int[7];
-		list[0] = 1000;
-		list[1] = 6;
-		list[2] = (52);
-		list[3] = (9);
-		list[4] = (11);
-		list[5] = (4);
-		list[6] = 1;
-		mergeSort(list, 0, list.length - 1);
-		for (int i = 0; i < list.length; i++) {
-			System.out.println(list[i]);
-		}
+		System.out.println(solution("Sat", 23));
 	}
 
-	static void merge(int Arr[], int start, int mid, int end) {
-
-		// create a temp array
-		int temp[] = new int[end - start + 1];
-
-		// crawlers for both intervals and for temp
-		int i = start;
-		int j = mid + 1;
-		int k = 0;
-
-		// traverse both arrays and in each iteration add smaller of both elements in
-		// temp
-		while (i <= mid && j <= end) {
-			if (Arr[i] <= Arr[j]) {
-				temp[k] = Arr[i];
-				k += 1;
-				i += 1;
-			} else {
-				temp[k] = Arr[j];
-				k += 1;
-				j += 1;
-			}
+	public static String solution(String S, int K) {
+		int total = K + getNumber(S);
+		if (total % 7 == 0) {
+			return "Mon";
+		} else if (total % 7 == 1) {
+			return "Tue";
+		} else if (total % 7 == 2) {
+			return "Wed";
+		} else if (total % 7 == 3) {
+			return "Thu";
+		} else if (total % 7 == 4) {
+			return "Fri";
+		} else if (total % 7 == 5) {
+			return "Sat";
+		} else if (total % 7 == 6) {
+			return "Sun";
 		}
-
-		// add elements left in the first interval
-		while (i <= mid) {
-			temp[k] = Arr[i];
-			k += 1;
-			i += 1;
-		}
-
-		// add elements left in the second interval
-		while (j <= end) {
-			temp[k] = Arr[j];
-			k += 1;
-			j += 1;
-		}
-
-		// copy temp to original interval
-		for (i = start; i <= end; i += 1) {
-			Arr[i] = temp[i - start];
-		}
+		return null;
 	}
 
-	// Arr is an array of integer type
-	// start and end are the starting and ending index of current interval of Arr
-
-	static void mergeSort(int Arr[], int start, int end) {
-
-		if (start < end) {
-			int mid = (start + end) / 2;
-			mergeSort(Arr, start, mid);
-			mergeSort(Arr, mid + 1, end);
-			merge(Arr, start, mid, end);
+	public static int getNumber(String S) {
+		int number = 0;
+		if (S.equalsIgnoreCase("Mon")) {
+			number = 0;
+		} else if (S.equalsIgnoreCase("Tue")) {
+			number = 1;
+		} else if (S.equalsIgnoreCase("Wed")) {
+			number = 2;
+		} else if (S.equalsIgnoreCase("Thu")) {
+			number = 3;
+		} else if (S.equalsIgnoreCase("Fri")) {
+			number = 4;
+		} else if (S.equalsIgnoreCase("Sat")) {
+			number = 5;
+		} else if (S.equalsIgnoreCase("Sun")) {
+			number = 6;
 		}
+		return number;
+
 	}
 
 }
--- JavaBookExercises/src/chapter12/Exercise_12_28.java
+++ JavaBookExercises/src/chapter12/Exercise_12_28.java
@@ -4,8 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Exercise_12_29 {
+public class Exercise_12_28 {
 	public static void main(String[] args) throws IOException {
+
 		Scanner inputScanner = new Scanner(System.in);
 		File folder = new File(inputScanner.next());
 		File[] files = null;
@@ -15,20 +16,17 @@ public class Exercise_12_29 {
 		if (folder.isDirectory()) {
 			files = folder.listFiles();
 		}
-		String fileName = "";
-		String subString = "";
-		String newFileName = "";
 		for (File file : files) {
 			String s = file.getName();
 			StringBuilder sb = new StringBuilder(s);
 
-			if (!sb.toString().matches("._0[\\d].*")) {
-				sb.insert(s.indexOf('_') + 1, "0");
+			if (!sb.toString().matches("Exercise0[\\d].*")) {
+
+				sb.insert(s.indexOf('_') - 1, "0");
 
 			}
-			file.renameTo(new File("Exercise12_29/"+sb.toString()));
+			file.renameTo(new File("Exercise12_28/" + sb.toString()));
 		}
 
 	}
-
 }
