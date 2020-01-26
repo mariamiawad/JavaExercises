1036c0c93c69aad41f5224e365dfa694e281776d
mariamiawad <mariam.i.awad@gmail.com>
2020-01-26 19:46:14 +0200

    CheckPermutations"

--- DataStructure/src/crackingcodinginterview/chapter01/CCI_01_02_CheckPermutations.java
+++ DataStructure/src/crackingcodinginterview/chapter01/CCI_01_02_CheckPermutations.java
@@ -1,10 +1,23 @@
 package crackingcodinginterview.chapter01;
 
+import java.util.Arrays;
+
 public class CCI_01_02_CheckPermutations {
-	
+
 	public boolean checkPermutation(String s1, String s2) {
-		// TODO - Implement this method
-		return false;
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		char[] chars1 = s1.toCharArray();
+		char[] chars2 = s2.toCharArray();
+		Arrays.sort(chars1);
+		Arrays.sort(chars2);
+		for (int i = 0; i < chars2.length; i++) {
+			if (chars1[i] != chars2[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
