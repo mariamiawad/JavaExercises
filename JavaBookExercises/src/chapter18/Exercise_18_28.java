498c2cd03a27ce917c33f0064cb45da2623a6a27
mariamiawad <mariam.i.awad@gmail.com>
2020-02-26 15:31:38 +0200

    Exercise_18_28

---
+++ JavaBookExercises/src/chapter18/Exercise_18_28.java
@@ -0,0 +1,29 @@
+package chapter18;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Exercise_18_28 {
+	public static void main(String[] args) {
+		// Prompt the user to enter a directory or a file
+		System.out.print("Enter a directory or a file: ");
+		Scanner input = new Scanner(System.in);
+		String directory = input.nextLine();
+
+		// Display the size
+		System.out.println(getSize(new File(directory)) + " bytes");
+	}
+
+	public static long getSize(File file) {
+		long size = 0; // Store the total size of all files
+
+		if (file.isDirectory()) {
+			File[] files = file.listFiles(); // All files and subdirectories
+			for (int i = 0;  i < files.length; i++) {
+				size += files[i].length(); // Recursive call
+			}
+		} 
+
+		return size;
+	}
+}
