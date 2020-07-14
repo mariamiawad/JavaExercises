eec6273ac557296f1eea06fe6656e2b31ce08763
Khaled Elbanna <engineer.khaled@gmail.com>
2020-07-15 00:34:26 +0100

    Graph search algorithms

---
+++ DataStructure/src/graph/Graph.java
@@ -0,0 +1,18 @@
+package graph;
+
+public class Graph {
+	
+	public Graph(int n) {
+		// TODO - Implement this
+	}
+	
+	public void addEdge(int u, int v) {
+		// TODO - Implement this
+	}
+	
+	public Iterable<Integer> adj(int v) {
+		// TODO - Implement this
+		return null;
+	}
+
+}
