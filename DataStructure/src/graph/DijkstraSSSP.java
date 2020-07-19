fb1e0fbf29febafe67ee24b7fe062b3e719d8de9
Khaled Elbanna <engineer.khaled@gmail.com>
2020-07-19 03:34:21 +0100

    Dijkstra and MST

---
+++ DataStructure/src/graph/DijkstraSSSP.java
@@ -0,0 +1,13 @@
+package graph;
+
+public class DijkstraSSSP {
+	
+	public void dijkstra(Graph G, int s) {
+		// TODO - Implement this
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ DataStructure/src/graph/MST.java
@@ -0,0 +1,17 @@
+package graph;
+
+public class MST {
+	
+	public void prim(Graph g) {
+		// TODO - Implement this method
+	}
+	
+	public void kruskal(Graph g) {
+		// TODO - Implement this method
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
